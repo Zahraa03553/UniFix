2781f9f3868dd8411aca2f7be210da9b14b9e449
ZahraaAbbas10 <202303553@student.polytechnic.bh>
2025-12-12 09:01:14 +0300

    login

--- UniFix/LoginViewController.swift
+++ UniFix/LoginViewController.swift
@@ -6,15 +6,68 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import FirebaseFirestore
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTxt: UITextField!
+    
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        loginButton.animateGradient(colors: [UIColor.primaryDarkGrey,UIColor.primaryGrey])
+    }
+   func shoeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
     
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    
+    guard let email = emailTxt.text, !email.isEmpty else {
+            shoeAlert(title: "Error", message: "Please enter email")
+            return
+        }
+        guard let password = passwordTxt.text, !password.isEmpty else {
+            shoeAlert(title: "Error", message: "Please enter password")
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {
+            authResult, error in
+            if let error = error {
+                self.shoeAlert(title: "Error", message: error.localizedDescription)
+            }
+            self.navigateBasedOnUserType()
+
+        }
+    }
+    func navigateBasedOnUserType() {
+        guard let userId = Auth.auth().currentUser?.uid else {return}
+        Firestore.firestore().collection("users").document(userId).getDocument {
+            snapshot, error in
+            if let data = snapshot?.data(),
+               let userType = data["userType"] as? String {
+                if userType == "admin" {
+                    self.performSegue(withIdentifier: "goToAdminDashboard", sender: self)
+                    
+                }
+                if userType == "student" {
+                    self.performSegue(withIdentifier: "goToStudentDashboard", sender: self)
+                }
+                if userType == "maintenanceTeam" {
+                    self.performSegue(withIdentifier: "goToMaintenanceTeam", sender: self)
+                }
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
--- UniFix/ViewController.swift
+++ UniFix/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        nextButton.animateGradient(colors: [UIColor.primaryDarkGrey,UIColor.primaryGrey])
+    }
 }
+extension UIButton {
+    func animateGradient(colors: [UIColor]) {
+        self.layer.sublayers?.removeAll(where: { $0 is CAGradientLayer })
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors.map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientLayer.cornerRadius = self.bounds.height / 2
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let animation = CABasicAnimation(keyPath: "startPoint")
+        animation.fromValue = CGPoint(x: 0.0, y: 0.5)
+        animation.toValue = CGPoint(x: 1.0, y: 0.5)
+        animation.duration = 30
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.repeatCount = .infinity
+        
+        let endAnimation = CABasicAnimation(keyPath: "endPoint")
+        endAnimation.fromValue = CGPoint(x: 1.0, y: 0.5)
+        endAnimation.toValue = CGPoint(x: 0.0, y: 0.5)
+        endAnimation.duration = 30
+        endAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        endAnimation.repeatCount = .infinity
+        
+        gradientLayer.add(animation, forKey: "startPointAnimation")
+        gradientLayer.add(endAnimation, forKey: "endPointAnimation")
+        
+        //
+        gradientLayer.locations = [0.0,0.5, 1.0,0.5 ,0.0]
+    }
+}
+
 

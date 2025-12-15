3f48723c706a4e9b861674e6417ceaa58d28ffe7
ZahraaAbbas10 <202303553@student.polytechnic.bh>
2025-12-15 17:08:57 +0300

    Add sign out

--- UniFix/RequestFormViewController.swift
+++ UniFix/RequestFormViewController.swift
@@ -7,22 +7,20 @@
 
 import UIKit
 
-class RequestFormViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
+class RequestFormViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
    
+    @IBOutlet weak var urgencyLevelTxt: UITextField!
     @IBOutlet weak var detailsTxt: UITextField!
     @IBOutlet weak var subjectTxt: UITextField!
     @IBOutlet weak var submit: UIButton!
     @IBOutlet weak var contactNumberTxt: UITextField!
     @IBOutlet weak var issueCategoryTxt: UITextField!
-    @IBOutlet weak var urgencyButton: UIButton!
-    let arrowImage = UIImageView(image: UIImage(systemName: "chevron.down"))
-    let dropDownTableView = UITableView()
-    let urgencyLevels = ["High", "Medium", "Low"]
-    var isDropDownOpen = false
     @IBOutlet weak var locationTxt: UITextField!
-    let errorMessage =  UILabel()
-    
-    
+  //  let errorMessage =  UILabel()
+    var myPickerView: UIPickerView!
+    var listoFCategories =  ["Electrical","Plumbing","IT","HVAC","Safety","Furniture","GroundKeeping","ACCessibility"]
+  //  var levelPickerView: UIPickerView!
+    var listLevels = ["High","Medium","Low"]
    override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,16 +29,17 @@ class RequestFormViewController: UIViewController, UIImagePickerControllerDelega
         subjectTxt.layer.borderColor = UIColor.primaryDarkGrey.cgColor
         subjectTxt.layer.cornerRadius = 8
         issueCategoryTxt.layer.borderWidth = 1
+       issueCategoryTxt.layer.cornerRadius = 8
         issueCategoryTxt.layer.borderColor = UIColor.primaryDarkGrey.cgColor
+       setupPickerView(_textField: issueCategoryTxt)
         locationTxt.layer.borderWidth = 1
         locationTxt.layer.borderColor = UIColor.primaryDarkGrey.cgColor
        locationTxt.layer.cornerRadius = 8
-       urgencyButton.addTarget(self, action: #selector(toggleDropDown), for: .touchUpInside)
-       dropDownTableView.isHidden = true
-       dropDownTableView.delegate = self
-       dropDownTableView.dataSource = self
-       dropDownTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
+       urgencyLevelTxt.layer.borderWidth = 1
+       urgencyLevelTxt.layer.borderColor = UIColor.primaryDarkGrey.cgColor
+       urgencyLevelTxt.layer.cornerRadius = 8
+       urgencyLevelTxt.layer.cornerRadius = 8
+       setupPickerView(_textField: urgencyLevelTxt)
         detailsTxt.layer.borderWidth = 1
         detailsTxt.layer.borderColor = UIColor.primaryDarkGrey.cgColor
        detailsTxt.layer.cornerRadius = 8
@@ -53,31 +52,42 @@ class RequestFormViewController: UIViewController, UIImagePickerControllerDelega
         submit.animateGradient(colors: [UIColor.primaryDarkGrey,UIColor.primaryGrey])
        
         }
-    @objc func toggleDropDown() {
-        isDropDownOpen.toggle()
-        dropDownTableView.isHidden = !isDropDownOpen
-        UIView.animate(withDuration: 0.3) {
-            self.arrowImage.transform = self.isDropDownOpen ? CGAffineTransform(rotationAngle: CGFloat.pi) : .identity
-            
-        }
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        urgencyLevels.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = urgencyLevels[indexPath.row]
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        urgencyButton.setTitle(urgencyLevels[indexPath.row], for: .normal)
-        toggleDropDown()
-    }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if isDropDownOpen {
-            toggleDropDown()
-        }
+    func setupPickerView(_textField: UITextField) {
+        myPickerView = UIPickerView()
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+        _textField.inputView = myPickerView
+       let toolbar = UIToolbar()
+       toolbar.sizeToFit()
+       let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donePressed))
+       toolbar.setItems([doneButton], animated: false)
+        _textField.inputAccessoryView = toolbar
+       
+   }
+@objc private func donePressed() {
+   view.endEditing(true)
+   }
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+   }
+   func pickerView(_ _pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+   return listoFCategories.count
+}
+func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       return listoFCategories[row]
+   }
+   func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       issueCategoryTxt.text = listoFCategories[row]
+   }
+//
+    func upickerView(_ _pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return listLevels.count
+ }
+    func upickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+           return listLevels[row]
+       }
+    func upickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        urgencyLevelTxt.text = listLevels[row]
     }
     func showAlert() {
         let alert = UIAlertController(title: "Error", message: " Please fill all the required fields", preferredStyle: .alert)
@@ -103,6 +113,8 @@ class RequestFormViewController: UIViewController, UIImagePickerControllerDelega
         }
     }
     
+    @IBAction func choserButtonTapped(_ sender: UIButton) {
+    }
     @IBAction func goToCameta(_ sender: UIButton) {
         openCamera()
     }
